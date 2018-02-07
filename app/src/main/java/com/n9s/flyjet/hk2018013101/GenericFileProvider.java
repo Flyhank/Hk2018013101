74c37f6c9ee05783b93d823887bb358059db5c7c
HKW <HKW@yahoo.com>
2018-02-07 13:26:33 +0800

    Initial commit

---
+++ app/src/main/java/com/n9s/flyjet/hk2018013101/GenericFileProvider.java
@@ -0,0 +1,12 @@
+package com.n9s.flyjet.hk2018013101;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Student on 2018/2/6.
+ */
+
+public class GenericFileProvider extends FileProvider
+{
+
+}
---
+++ app/src/main/java/com/n9s/flyjet/hk2018013101/MainActivity.java
@@ -0,0 +1,121 @@
+package com.n9s.flyjet.hk2018013101;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.renderscript.ScriptGroup;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+{
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
+    }
+    public void click1(View v)
+    {
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);    //啟動相機
+        startActivityForResult(it, 123);
+    }
+
+    public void click2(View v)
+    {
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(getExternalFilesDir("PHOTO"), "myphoto.jpg");
+        Uri photoUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName()
+                + ".my.package.name.provider", f);
+        it.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(it, 456);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                Bundle pBundle = data.getExtras(); //將拍照相片("縮圖)放入imageView顯示
+                Bitmap bmp = (Bitmap) pBundle.get("data");
+                img.setImageBitmap(bmp);
+            }
+        }
+        if (requestCode == 456)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                //將storage相片("縮圖)放入imageView顯示內取出,因為bitmap格式未壓縮,恐塞爆memory
+                //File f = new File(getExternalFilesDir("PHOTO"), "myphoto.jpg");
+                //Bitmap bmp = BitmapFactory.decodeFile(f.getAbsolutePath());
+                //img.setImageBitmap(bmp);
+                //改為以下寫法較好,
+
+                File f = new File(getExternalFilesDir("PHOTO"), "myphoto.jpg");
+                Uri photoUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName()
+                        + ".my.package.name.provider", f);
+                try {
+                    // InputStream is = new FileInputStream(photoUri);
+                    InputStream is = getContentResolver().openInputStream(photoUri);
+                    Log.d("BMP", "Can READ:" + is.available());
+                    Bitmap bmp = getFitImage(is);
+                    img.setImageBitmap(bmp);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+    public static Bitmap getFitImage(InputStream is)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        byte[] bytes = new byte[0];
+        try {
+            bytes = readStream(is);
+            //BitmapFactory.decodeStream(inputStream, null, options);
+            Log.d("BMP", "byte length:" + bytes.length);
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+            System.gc();
+            // Log.d("BMP", "Size:" + bmp.getByteCount());
+            return bmp;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static byte[] readStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        outStream.close();
+        inStream.close();
+        return outStream.toByteArray();
+    }
+}
